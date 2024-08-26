28c92d365f9a42aa8b33a1fd6a4f60648d441e7f
Jarvi Zambrano <jarvi.zambrano@mercadolibre.com.co>
2024-08-26 15:16:43 -0500

    test-1

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,2 +1,3 @@
 -- test rp
-insert into rp (id, name, description) values('localhost', 'example1', 'example1');
+insert into rp (id, name, description) values('localhost1111', 'example1-modify', 'example1-modify');
+insert into rp (id, name, description) values('localhost', 'example2-modify', 'example2-modify');
